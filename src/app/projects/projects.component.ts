1a2e7d3415d83b547ac1a9a14fccd16c612516aa
Aryan Patel <appatel@augmentedtechlabs.com>
2022-09-11 17:00:39 -0400

    build error fix

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Apollo} from "apollo-angular";
-import gql from 'graphql-tag';
 import { environment } from 'src/environments/environment';
 import { ButtonService } from '../button.service';
 
